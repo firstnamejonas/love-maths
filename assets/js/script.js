dc0afd97c2dff8a53838ef3c4164afe39211199e
Jonas Hessling <firstname.jonas@icloud.com>
2023-12-13 12:01:50 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// Die Funktion runGame sorgt dafür, dass das Game läuft sobald ein User auf die Seite kommt!
+function runGame() {
+
+}
+
+// Die Funktion checkAnswer sorgt dafür, dass das Ergebnis überprüft wird
+function checkAnswer() {
+
+}
+
+// Die Funktion calculateCorrectAnswer sorgt dafür, dass das richtige Ergebnis angezeigt wird
+function calculateCorrectAnswer() {
+
+}
+
+// Die Funktion calculateCorrectAnswer sorgt dafür, dass  die richigen Antworten Score hochgesetzt wird
+function calculateCorrectAnswer() {
+
+}
+
+// Die Funktion calculateCorrectAnswer sorgt dafür, dass der falschen Antworten Score hochgeht
+function calculateCorrectAnswer() {
+
+}
+
+// Die Funktion calculateCorrectAnswer sorgt dafür, dass Plus Fragen angezeigt werden
+function calculateCorrectAnswer() {
+
+}
+
+// Die Funktion calculateCorrectAnswer sorgt dafür, dass dass Minus Fragen angezeigt werden
+function calculateCorrectAnswer() {
+
+}
+
+// Die Funktion calculateCorrectAnswer sorgt dafür, dass dass Mal Fragen angezeigt werden
+function calculateCorrectAnswer() {
+
+}
