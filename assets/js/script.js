b1afa6b2d22819b867ce8e045fa24ec893413fd2
Jonas Hessling <firstname.jonas@icloud.com>
2023-12-14 10:25:48 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2)
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2)
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2)
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -62,6 +64,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -92,8 +96,10 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 // Die Funktion displaySubtractQuestion sorgt dafür, dass dass Minus Fragen angezeigt werden
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 
 // Die Funktion displayMultiplyQuestion sorgt dafür, dass dass Mal Fragen angezeigt werden
